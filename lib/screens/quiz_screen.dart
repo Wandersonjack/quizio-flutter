8e4046371e81663cd37baefa52f926f0ddcc6ece
wandersonjack <wander8jackson@gmail.com>
2019-12-28 20:42:18 +0200

    App structure for the quiz

---
+++ lib/screens/quiz_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class QuizScreen extends StatefulWidget {
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
