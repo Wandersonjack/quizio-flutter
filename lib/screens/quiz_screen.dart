efef3c03fc60fd9fc72e1bb88fd69739bc236ab6
wandersonjack <wander8jackson@gmail.com>
2019-12-30 20:39:26 +0200

    quiz screen buttons and question

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -6,13 +6,84 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+  List<Icon> scoreKeeper = [
+    //icons comes here
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-
-        ],
+      backgroundColor: Colors.black54,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: Text(
+                    "Here comes the question ",
+                    style: TextStyle(fontSize: 25.0, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      scoreKeeper.add(
+                        Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ),
+                      );
+                    });
+                  },
+                  color: Colors.green,
+                  child: Text(
+                    "Yes",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      scoreKeeper.add(
+                        Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ),
+                      );
+                    });
+                  },
+                  color: Colors.red,
+                  child: Text(
+                    "No",
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            //TODO: add row here as your score keeper
+            Row(
+              children: scoreKeeper,
+            )
+          ],
+        ),
       ),
     );
   }
